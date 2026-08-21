0b8db7d96546008c53706fa157807905b1aed4ab
Muhammad Alyousef <muhammadabdalrahman2003@gmail.com>
2026-08-22 02:11:52 +0300

    new

--- src/components/sidebar/SidebarRoutes.tsx
+++ src/components/sidebar/SidebarRoutes.tsx
@@ -1,40 +1,19 @@
-// import { ROUTE_PERMISSIONS_MAP } from "@/lib/constants";
 import useAuth from "@/modules/auth/store/authStore";
 import {
-  // Activity,
   BookOpen,
   Folder,
-  // FolderOpen,
-  // Headphones,
   Home,
-  // Images,
-  // InfoIcon,
-  // Layers,
-  // MapPin,
-  // MessageSquare,
-  // Package,
-  // Paperclip,
-  // Settings,
   Shield,
-  Signal,
-  // Star,
   Store,
-  // Tag,
   User,
   Users,
   WalletCards,
 } from "lucide-react";
 import { useTranslations } from "next-intl";
 import { MenuItem } from "./app-sidebar";
-// import { usePermissionStore } from "@/hooks/usePermissionStore";
-
-// src/routes/sidebarRoutes.ts
 export function SidebarRoutes(): MenuItem[] {
   const t = useTranslations("Sidebar");
-  // const hasPermission = usePermissionStore((state) => state.hasPermission);
   const user = useAuth((state) => state?.user);
-  // const isSuperAdmin =
-  // console.log("user?.is_instructor", user?.is_instructor);
 
   const routes: MenuItem[] = [
     // Home
@@ -45,11 +24,6 @@ export function SidebarRoutes(): MenuItem[] {
     },
     ...(!user?.is_instructor
       ? [
-          // {
-          //   href: "/dashboard/analytics",
-          //   label: t("navigation.analytics"),
-          //   icon: Signal,
-          // },
           {
             href: "/dashboard/categories",
             label: t("navigation.categories"),
@@ -113,40 +87,8 @@ export function SidebarRoutes(): MenuItem[] {
             searchParams: { role: "STUDENT", page: "1" },
           },
         ]),
-    // {
-    //   href: "/dashboard/settings",
-    //   label: t("navigation.settings"),
-    //   icon: Settings,
-    // },
   ];
 
   return routes;
 
-  // const filterRoutes = (items: MenuItem[]): MenuItem[] => {
-  //   return items
-  //     .map((item) => {
-  //       const key = item.id || item.menuType || item.href?.split("?")[0] || "";
-  //       const perms = ROUTE_PERMISSIONS_MAP[key] || [];
-
-  //       if (item.children) {
-  //         const filteredChildren = filterRoutes(item.children);
-  //         if (filteredChildren.length === 0) {
-  //           return null;
-  //         }
-  //         return { ...item, children: filteredChildren };
-  //       }
-
-  //       const hasAccess =
-  //         perms.length === 0 || perms.some((perm) => hasPermission(perm));
-
-  //       // console.log(
-  //       //   `Item: ${item.label}, Key: ${key}, Perms: ${perms}, Has Access: ${hasAccess}`
-  //       // );
-
-  //       return hasAccess ? item : null;
-  //     })
-  //     .filter(Boolean) as MenuItem[];
-  // };
-
-  // return filterRoutes(routes);
 }
